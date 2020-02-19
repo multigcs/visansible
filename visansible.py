177b14671868df8ac9b1f31f238b42749a775a9d
Oliver Dippel <o.dippel@gmx.de>
2020-02-19 23:09:50 +0100

    some fixes

--- visansible.py
+++ visansible.py
@@ -68,7 +68,7 @@ class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
 				os.mkdir("facts")
 			if not os.path.exists("facts/hist_" + str(timestamp)):
 				os.mkdir("facts//hist_" + str(timestamp))
-			command = ['ansible', '-i', 'inventory.cfg', opts["host"], '-m', 'setup', '--tree', 'facts/hist_' + str(timestamp)]
+			command = ['ansible', '-i', self.rf.inventory["file"], opts["host"], '-m', 'setup', '--tree', 'facts/hist_' + str(timestamp)]
 			result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 			html = HtmlPage("Visansible", "Rescan", "");
 			html.add("<b>command:</b>")
